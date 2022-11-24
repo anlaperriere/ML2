1cf92d0b16f849f09460f721a653205815de18d4
Guytangzz <tanguy.desjardin@epfl.ch>
2022-11-24 19:15:46 +0100

    added feature assesment function in helpers

--- scripts/.ipynb_checkpoints/helpers-checkpoint.py
+++ scripts/.ipynb_checkpoints/helpers-checkpoint.py
@@ -1,6 +1,7 @@
 import matplotlib.image as mpimg
 import numpy as np
 import matplotlib.pyplot as plt
+import os, sys
 
 def load_image(infilename):
     data = mpimg.imread(infilename)
@@ -44,3 +45,51 @@ def img_crop(im, w, h):
                 im_patch = im[j : j + w, i : i + h, :]
             list_patches.append(im_patch)
     return list_patches
+
+
+def extract_features(img):
+    """
+    Extract 6-dimensional features consisting of average RGB color as well as variance
+    """
+    feat_m = np.mean(img, axis=(0, 1))
+    feat_v = np.var(img, axis=(0, 1))
+    feat = np.append(feat_m, feat_v)
+    return feat
+
+
+def extract_features_2d(img):
+    """
+    Extract 2-dimensional features consisting of average gray color as well as variance
+    """
+    feat_m = np.mean(img)
+    feat_v = np.var(img)
+    feat = np.append(feat_m, feat_v)
+    return feat
+
+
+def extract_img_features(filename):
+    """
+    Extract features for a given image
+    """
+    img = load_image(filename)
+    img_patches = img_crop(img, patch_size, patch_size)
+    X = np.asarray(
+        [extract_features_2d(img_patches[i]) for i in range(len(img_patches))]
+    )
+    return X
+
+
+def load_training_data(folder_dir, n=100):
+    """
+    Loads all the training files
+    """
+    img_dir = folder_dir + "images/"
+    files = os.listdir(img_dir)
+    n = min(n, len(files))  # Load maximum 20 images
+    print("Loading " + str(n) + " images")
+    imgs = [load_image(img_dir + files[i]) for i in range(n)]
+
+    gnd_dir = folder_dir + "groundtruth/"
+    print("Loading " + str(n) + " images")
+    gnd_imgs = [load_image(gnd_dir + files[i]) for i in range(n)]
+    return imgs, gnd_imgs
